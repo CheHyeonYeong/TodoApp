e8b91a16381a2ac9d0edbabf2d6ead2598a803a9
CheHyeonyeong <gusdudco6@naver.com>
2023-03-22 13:10:56 +0900

    3/22 어노테이션 사용하여 기초 틀 제공

--- src/main/java/com/example/demo/controller/ToDoController.java
+++ src/main/java/com/example/demo/controller/ToDoController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
+import com.example.demo.service.TodoService;
 import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +22,20 @@ import java.util.List;
 @RestController
 @RequestMapping("todo")
 public class ToDoController {
-    
+
+    @Autowired
+    private TodoService service;
+    @GetMapping("/test1")
+    public ResponseEntity<?> testTodo1(){
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+
     //todo 메서드 작성하기
+
     @GetMapping("/testTodo")
     public ResponseEntity<?> testTodo (){
         //생성자 쓰기
